e4e79531678ff59f5167c84fba27d01d3efb61a6
Aravinth009 <aramechvsb@gmail.com>
2022-04-30 12:08:15 +0530

    A works

--- src/org/sample2/AxisBank.java
+++ src/org/sample2/AxisBank.java
@@ -3,10 +3,10 @@ package org.sample2;
 import java.util.LinkedList;
 import java.util.List;
 
-public class AxisBank{
+public class AxisBank {
 	public static void main(String[] args) {
 		List li = new LinkedList<>();
-		
+
 		li.add(10);
 		li.add("Java");
 		li.add('a');
@@ -16,54 +16,50 @@ public class AxisBank{
 		li.add(true);
 		li.add(10);
 		System.out.println(li);
-	int size = li.size();
-	System.out.println(size);
-	Object object = li.get(5);
-	System.out.println( object);
-	  li.remove(3);
-	System.out.println(li);
-	li.add(2, 300);
-	System.out.println(li);
-	li.set(1, "selenium");
-	System.out.println(li);
-	boolean contains = li.contains(10);
-	System.out.println(contains);
-	int indexOf = li.indexOf(10);
-	System.out.println(indexOf);
-	int a = li.lastIndexOf(10);
-	System.out.println(a);
-	int o = li.indexOf("python");
-	System.out.println(o);
-	boolean empty = li.isEmpty();
-	System.out.println(empty);	
-	System.out.println("*******************");
-	//to iterate list
-	
-	System.out.println("for loop");
-	
-	for (int i = 0; i < li.size(); i++) {
-		System.out.println(li.get(i));
-		
-		
+		int size = li.size();
+		System.out.println(size);
+		Object object = li.get(5);
+		System.out.println(object);
+		li.remove(3);
+		System.out.println(li);
+		li.add(2, 300);
+		System.out.println(li);
+		li.set(1, "selenium");
+		System.out.println(li);
+		boolean contains = li.contains(10);
+		System.out.println(contains);
+		int indexOf = li.indexOf(10);
+		System.out.println(indexOf);
+		int a = li.lastIndexOf(10);
+		System.out.println(a);
+		int o = li.indexOf("python");
+		System.out.println(o);
+		boolean empty = li.isEmpty();
+		System.out.println(empty);
+		System.out.println("*******************");
+		// to iterate list
+
+		System.out.println("for loop");
+
+		for (int i = 0; i < li.size(); i++) {
+			System.out.println(li.get(i));
+
+		}
+		System.out.println("*******************");
+		System.out.println("enhanced for loop");
+		for (Object x : li) {
+			System.out.println(x);
+
+		}
+System.out.println("aravinth");
+System.out.println("ara@123");
+System.out.println("password");
+System.out.println("Jhansi");
+System.out.println("Brindha");
+System.out.println("Boopathi");
 	}
-	System.out.println("*******************");
-System.out.println("enhanced for loop");
-	for (Object x : li) {
-		System.out.println(x);
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	}
-	
-	
-	
+
 
 
 }
+
